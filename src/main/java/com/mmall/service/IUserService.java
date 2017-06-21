e22c773e6ef08334e0f36a9d54cf2681c7126b27
neko <neko.bj4501@gmail.com>
2017-06-21 12:44:52 +0800

    UserService

---
+++ src/main/java/com/mmall/service/IUserService.java
@@ -0,0 +1,23 @@
+package com.mmall.service;
+
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+
+/**
+ * Created by neko on 2017/6/20.
+ */
+public interface IUserService {
+
+    ServerResponse<User> login(String username, String password);
+
+    ServerResponse<String> register(User user);
+
+    ServerResponse<String> checkVaild(String str,String type);
+
+    ServerResponse selectQuestion(String username);
+
+    ServerResponse<String> checkAnswer(String username,String question,String answer);
+
+    ServerResponse<String> forgetResetPassword(String username,String passwordNew,String forgetToken);
+
+}
