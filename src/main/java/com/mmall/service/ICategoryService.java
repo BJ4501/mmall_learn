6cf615aee801d217242150a1fed6b81d7e64258f
neko <neko.bj4501@gmail.com>
2017-06-22 00:08:42 +0800

    CategoryService--ok

---
+++ src/main/java/com/mmall/service/ICategoryService.java
@@ -0,0 +1,21 @@
+package com.mmall.service;
+
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.Category;
+
+import java.util.List;
+
+/**
+ * Created by neko on 2017/6/21.
+ */
+public interface ICategoryService {
+
+    ServerResponse addCategory(String categoryName, Integer parentId);
+
+    ServerResponse updateCategoryName(Integer categoryId,String categoryName);
+
+    ServerResponse<List<Category>> getChildrenParallelCategory(Integer categoryId);
+
+    ServerResponse selectCategoryAndChildrenById(Integer categoryId);
+
+}
